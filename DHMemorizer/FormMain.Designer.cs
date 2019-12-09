750cbf94b258431b34b21c1c1e073efac535d1c6
Dohyun Lee <lonelytrip@nate.com>
2019-12-09 10:14:52 +0900

    초기 커밋.

---
+++ DHMemorizer/FormMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DHMemorizer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelIcon = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStripMain.SuspendLayout();
+            this.menuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelIcon});
+            resources.ApplyResources(this.statusStripMain, "statusStripMain");
+            this.statusStripMain.Name = "statusStripMain";
+            // 
+            // toolStripStatusLabelIcon
+            // 
+            this.toolStripStatusLabelIcon.IsLink = true;
+            this.toolStripStatusLabelIcon.Name = "toolStripStatusLabelIcon";
+            resources.ApplyResources(this.toolStripStatusLabelIcon, "toolStripStatusLabelIcon");
+            this.toolStripStatusLabelIcon.Click += new System.EventHandler(this.toolStripStatusLabelIcon_Click);
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+            resources.ApplyResources(this.menuStripMain, "menuStripMain");
+            this.menuStripMain.Name = "menuStripMain";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            resources.ApplyResources(this.파일ToolStripMenuItem, "파일ToolStripMenuItem");
+            // 
+            // FormMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Name = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelIcon;
+    }
+}
+
---
+++ DHMemorizer/FormMain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace DHMemorizer
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// FormMain Load 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //메인폼의 이름
+            this.Text = Properties.Resource.Internationalization.FormMain.DHMemorizer;
+        }
+
+        /// <summary>
+        /// toolStripStatusLabelIcon Click 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripStatusLabelIcon_Click(object sender, EventArgs e)
+        {
+            VisitLink("https://icons8.com");
+        }
+
+        private void VisitLink(string url)
+        {
+            if (null == url)
+            {
+                return;
+            }
+            toolStripStatusLabelIcon.LinkVisited = true;
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}
